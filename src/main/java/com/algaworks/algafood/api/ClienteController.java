f7ae04e9aaf6abd2c2bfd432b8e20f4a2e86daa6
Bruno Banguela <bjbmiguel@gmail.com>
2024-10-06 18:29:35 +0100

    26.4. Configurando múltiplas documentações em um só projeto

---
+++ src/main/java/com/algaworks/algafood/api/ClienteController.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood.api;
+
+import com.algaworks.algafood.domain.model.Pedido;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.ListResourceBundle;
+
+@RestController
+public class ClienteController {
+
+    @GetMapping("/clientes/v1/pedidos")
+    List<Pedido> listar(){
+
+        return Collections.EMPTY_LIST;
+    }
+}
--- src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -4,11 +4,15 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringDocConfig {
+
+    /*
+
 
     @Bean
     public OpenAPI openAPI() {
@@ -25,6 +29,55 @@ public class SpringDocConfig {
                         .description("AlgaWorks")
                         .url("https://algaworks.com")
                 );
+    }
+  */
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi(){
+        return GroupedOpenApi.builder()
+                .group("AlgaFood API Admin")
+                .pathsToMatch("/v1/**")
+                .addOpenApiCustomiser(openApi -> {
+
+                    openApi.info(new Info()
+                            .title("AlgaFood API")
+                            .version("v1")
+                            .description("REST API do AlgaFood")
+                            .license(new License()
+                                    .name("Apache 2.0")
+                                    .url("http://springdoc.com")
+                            )
+                    ).externalDocs(new ExternalDocumentation()
+                            .description("AlgaWorks")
+                            .url("https://algaworks.com")
+                    );
+                })
+
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi groupedOpenApiCliente(){
+        return GroupedOpenApi.builder()
+                .group("AlgaFood API Cliente")
+                .pathsToMatch("/clientes/v1/**")
+                .addOpenApiCustomiser(openApi -> {
+
+                    openApi.info(new Info()
+                            .title("AlgaFood API Cliente")
+                            .version("v1")
+                            .description("REST API do AlgaFood")
+                            .license(new License()
+                                    .name("Apache 2.0")
+                                    .url("http://springdoc.com")
+                            )
+                    ).externalDocs(new ExternalDocumentation()
+                            .description("AlgaWorks")
+                            .url("https://algaworks.com")
+                    );
+                })
+
+                .build();
     }
 
 }
